6eb4027943c1b4c80c3399c44d81c83382a441a8
Mukhtartg <mukhtartanimugarba@gmail.com>
2025-04-27 15:44:38 +0100

    Update frontend package.json to include react-query dependency

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from 'next/app';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp; 
